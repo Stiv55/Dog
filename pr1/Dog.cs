53eb6b0404595720118458689277ea87e9755c3f
Stiv55 <privet.1234562013@yandex.ru>
2021-04-19 11:53:06 +0300

    Edit Dog Class!

--- pr1/Dog.cs
+++ pr1/Dog.cs
@@ -7,7 +7,58 @@ namespace pr1
     class Dog
     {
         private string name;
-        private int breed;
+        private string breed;
         private float age;
+        public Dog()
+        {
+            name = "";
+            breed = "";
+            age = 0;
+        }
+        public void ShowCost()
+        {
+            Console.WriteLine("The current stuff breed = {0}", breed);
+        }
+        public void ShowName()
+        {
+            Console.WriteLine("The current stuff name is {0}", name);
+        }
+        public void ShowAmount()
+        {
+            Console.WriteLine("The current stuff age = {0}", age);
+        }
+        public string NAME
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string BREED
+        {
+            get
+            {
+                return breed;
+            }
+            set
+            {
+                breed = value;
+            }
+        }
+        public float AGE
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
     }
 }
